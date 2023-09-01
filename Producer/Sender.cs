df81756fc07eacd07000a8c333351a6e49607b43
Juan Carlos <juan.castro.socla@gmail.com>
2023-09-01 17:21:30 -0500

    Producer

--- Producer/Sender.cs
+++ Producer/Sender.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+	HostName = "localhost",
+	UserName = "jucastro",
+	Password = "JuanCastro2023",
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+	channel.QueueDeclare("VaxiQueue", false, false, false, null);
+
+	var message = "Bienvenido al curso de RabbitMq y .NET";
+	var body = Encoding.UTF8.GetBytes(message);
+
+	channel.BasicPublish("", "VaxiQueue",null,body);
+	Console.WriteLine("El mensaje fue enviado {0}", message);
+
+}
+
+Console.WriteLine("Presiona [enter] para salir de la aplicación");
+Console.ReadLine();
